49da9749cb9d6c951c5db2c259b40b4a0194af4b
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-14 17:26:51 +0900

    Add files via upload

---
+++ 04/Hello04.java
@@ -0,0 +1,10 @@
+/**
+ * 순차적인 프로그램 작성하기 예제
+ */
+
+public class Hello04{
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("I'm a new java programmer.");
+    }
+}
---
+++ 06/Areatest.java
@@ -0,0 +1,15 @@
+/**
+ * 공학적 계산에서는 부동소수점형 사용
+ */
+
+public class Areatest{
+    public static void main(String args[]){
+        double radius,pi,area;
+
+        radius=5.;
+        pi=3.141592;
+        area=pi*radius*radius;
+
+        System.out.println("원의 면적은 :"+area);
+    }
+}
