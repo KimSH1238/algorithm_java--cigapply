89e40342fb04cf1da81243031a79b4398d5a323f
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-14 18:30:40 +0900

    Add files via upload

---
+++ cjg/VarInitTest.java
@@ -0,0 +1,13 @@
+public class VarInitTest
+{
+    public static void main(String args[])
+    {
+        int index=0;
+
+        for (int i=0;i<5;i++)
+        {
+            index=index+1;
+            System.out.println("index : "+index);
+        }
+    }
+}
