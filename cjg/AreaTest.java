01dedfda619bd24491632be6d32a4571d471c1b9
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-14 18:03:40 +0900

    Add files via upload

---
+++ cjg/AreaTest.java
@@ -0,0 +1,11 @@
+public class AreaTest
+{
+    public static void main(String args[])
+    {
+        double radius,area;
+        radius=5.0;
+        area=3.141592*Math.pow(radius,2);
+
+        System.out.println("원의 면적은 :" +area+"\n");
+    }
+}
