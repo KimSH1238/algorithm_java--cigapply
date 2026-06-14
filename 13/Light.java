4adc74d1969d2e0c4a4852c734b87d18d30a343b
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-14 17:33:32 +0900

    Add files via upload

---
+++ 13/Light.java
@@ -0,0 +1,11 @@
+public class Light
+{
+    public static void main(String[] args)
+    {
+        long distance;
+        long lightspeed = 300000;
+        distance=lightspeed*365*24*3600;
+
+        System.out.println("빛이 1년동안 가는 거리 : "+distance+"km \n");
+    }
+}
